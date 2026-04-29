c8ac8d05604164bc9967b74ab50d38f2ae490f1f
shiny deepika balanta <shinydeepikabalanta@gmail.com>
2026-04-29 14:26:24 +0530

    transfer

--- aksh.js
+++ aksh.js
@@ -1,1 +1,11 @@
+const input = document.getElementById('username-input');
+  const btn = document.getElementById('start-btn');
+
+  input.addEventListener('input', () => {
+    const val = input.value.trim();
+    const isValid = val.length >= 3;
+
+    btn.disabled = !isValid;
+    btn.classList.toggle('ready', isValid);
+  });
 
